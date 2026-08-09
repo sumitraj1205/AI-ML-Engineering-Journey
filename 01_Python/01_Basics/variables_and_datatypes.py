356855cf46ff78f14dc4dc2bbcce3404f03f057a
Sumit Raj <sumitraj93344@gmail.com>
2026-08-09 12:15:50 +0530

    Add Python variables and data types example

---
+++ 01_Python/01_Basics/variables_and_datatypes.py
@@ -0,0 +1,14 @@
+name = "Sumit"
+age = 21
+height = 6.0
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
