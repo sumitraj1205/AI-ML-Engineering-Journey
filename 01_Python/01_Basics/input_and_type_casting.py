3ce59d01f1035688163096dd1a00c7c949158dc0
Sumit Raj <sumitraj93344@gmail.com>
2026-08-09 12:20:04 +0530

    Add input and type casting examples

---
+++ 01_Python/01_Basics/input_and_type_casting.py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Name:", name)
+print("Age:", age)
+
+age = int(age)
+
+print("Age next year:", age + 1)
+
+number = "100"
+number = int(number)
+
+decimal_number = "10.5"
+decimal_number = float(decimal_number)
+
+print(number)
+print(decimal_number)
