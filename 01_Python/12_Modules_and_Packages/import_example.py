d69e9f001d7e991e4594ba0a5c551efc1c56c295
sumitraj1205 <sumitraj93344@gmail.com>
2026-08-12 12:04:26 +0530

    Add Python modules and packages programs

---
+++ 01_Python/12_Modules_and_Packages/import_example.py
@@ -0,0 +1,9 @@
+import custom_module
+
+a = 20
+b = 10
+
+print("Addition:", custom_module.add(a, b))
+print("Subtraction:", custom_module.subtract(a, b))
+print("Multiplication:", custom_module.multiply(a, b))
+print("Division:", custom_module.divide(a, b))
---
+++ 01_Python/12_Modules_and_Packages/math_module.py
@@ -0,0 +1,13 @@
+import math
+
+number = float(input("Enter a number: "))
+
+print("Square root:", math.sqrt(number))
+print("Power:", math.pow(number, 2))
+print("Ceiling:", math.ceil(number))
+print("Floor:", math.floor(number))
+print("Absolute value:", math.fabs(number))
+print("Factorial:", math.factorial(int(number)))
+
+print("Value of pi:", math.pi)
+print("Value of e:", math.e)
